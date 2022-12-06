97bce57c8ae2a31aea8f59d91278a349bd5d5c12
Ryan Jin <ryanjin007@gmail.com>
2022-12-06 06:58:00 +0100

    Finish day6

---
+++ 2022/day6/part1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from collections import Counter
+
+# --------------------- Part 1 ---------------------
+# Reading each of the lines in the text file into the list
+lines = []
+with open('input') as f:
+    lines = f.readlines()
+
+def dups(str):
+    """Check for duplicate char in strings"""
+
+    # Counter(str) returns a dict where the key
+    # is the char of the string and value is number
+    # of occurences
+    for key, val in Counter(str).items():
+        # Check to see if there are any duplicates
+        if val > 1:
+            return True
+    return False
+
+row = lines[0]
+for i in range(len(row) - 3):
+    if not dups(row[i:i + 4]):
+        print(i + 4)
+        break
